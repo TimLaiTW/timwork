dff0b5677cd0cc09763c14e25ab4ba5c5e834580
tailinl <tailinl@google.com>
2023-05-31 15:43:21 +0800

    add pages components

---
+++ src/app/pages/contact/contact.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.html',
+  styleUrls: ['./contact.scss']
+})
+export class ContactPage {
+
+}
---
+++ src/app/pages/work/work.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work',
+  templateUrl: './work.html',
+  styleUrls: ['./work.scss']
+})
+export class WorkPage {
+
+}
