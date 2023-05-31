70934305e57ce94fe89e50af30db49ce95533a5d
tailinl <tailinl@google.com>
2023-05-31 12:35:46 +0800

    site header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { MaterialModule } from '../material.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Toolbar } from './components/toolbar/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Toolbar
   ],
   imports: [
     BrowserModule,
---
+++ src/static_string.ts
@@ -0,0 +1,6 @@
+export const SiteTitle = "timwork";
+export enum PageTitle {
+    WORK = "work",
+    ABOUT = "about",
+    CONTACT = "contact"
+};
